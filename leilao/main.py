7f7b57c982f670ccc64763f08de4d9ef7c0053fa
Thiago Ferreira <thiaago.dev@gmail.com>
2021-06-23 18:30:10 -0300

    Implementando avaliador de leilões

---
+++ leilao/main.py
@@ -0,0 +1,20 @@
+from dominio import Leilao, Lance, Usuario, Avaliador
+
+cleiton = Usuario('Cleiton')
+jorge = Usuario('Jorge')
+
+lance_do_cleiton = Lance(cleiton, 150)
+lance_do_jorge = Lance(jorge, 100)
+
+leilao = Leilao('Celular')
+
+leilao.lances.append(lance_do_cleiton)
+leilao.lances.append(lance_do_jorge)
+
+for lance in leilao.lances:
+    print(f'O usuário {lance.usuario.nome} deu um lance de {lance.valor}')
+
+avaliador = Avaliador()
+avaliador.avalia(leilao)
+
+print(f'O menor lance foi de {avaliador.menor_lance} e o maior lance foi {avaliador.maior_lance}')
